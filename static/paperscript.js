d0df69f2d975b4bc797f70eb157c3f590cc4c032
Maido Käära <maido@producement.com>
2025-07-04 11:39:46 +0300

    Refactor

--- static/paperscript.js
+++ static/paperscript.js
@@ -6,30 +6,115 @@ project.currentStyle = {
 	strokeWidth: 2
 };
 
-var xScale = view.size.width / 400;
-var yScale = view.size.height / 300;
+var xScale = 1;
+var yScale = 1;
 
-var points = [
-	[25, 25],
-	[95, 30],
-	[165, 100],
-	[235, 200],
-	[305, 200],
-	[375, 50]
-];
-
-var graph = new Path({ segments: points });
-graph.smooth({ type: "catmull-rom" });
-
-var fillPath = new Path({
-	segments: points.concat([
+var graph = new Path({
+	segments: [
+		[25, 275],
+		[95, 275],
+		[165, 275],
+		[235, 275],
+		[305, 275],
+		[375, 275],
 		[375, 275],
 		[25, 275]
-	]),
+	],
 	closed: true
 });
 
-var axes = new Path({
+var curveSegments = graph.segments.slice(0, 6);
+
+function smoothen() {
+	var i;
+	for (i = 0; i < graph.segments.length; i++) {
+		if (i > 0 && i < curveSegments.length - 1) {
+			// Smooth only the middle curve segments
+			graph.segments[i].smooth({ type: "catmull-rom" });
+		} else {
+			// Keep first curve point, last curve point, and bottom segments sharp
+			graph.segments[i].smooth = false;
+		}
+	}
+}
+
+function createGradient() {
+	var gradientStops = [];
+	var xStart = curveSegments[0].point.x;
+	var xEnd = curveSegments[curveSegments.length - 1].point.x;
+	var xRange = xEnd - xStart;
+
+	// Define base colors
+	var hot = new Color(1.0, 1.0, 0.0);
+	var cold = new Color(0.0, 0.4, 1.0);
+	var neutral = new Color(0.9, 0.1, 0.0);
+
+	curveSegments.forEach(function (segment) {
+		var point = segment.point;
+		var t = point.y / (170 * yScale);
+		t = Math.max(0, Math.min(1, t));
+
+		var r, g, b, f;
+
+		// First half: hot → neutral
+		if (t < 0.5) {
+			f = t / 0.5;
+			r = hot.red + (neutral.red - hot.red) * f;
+			g = hot.green + (neutral.green - hot.green) * f;
+			b = hot.blue + (neutral.blue - hot.blue) * f;
+		}
+		// Second half: neutral → cold
+		else {
+			f = (t - 0.5) / 0.5;
+			r = neutral.red + (cold.red - neutral.red) * f;
+			g = neutral.green + (cold.green - neutral.green) * f;
+			b = neutral.blue + (cold.blue - neutral.blue) * f;
+		}
+
+		gradientStops.push(
+			new GradientStop(new Color(r, g, b), (point.x - xStart) / xRange)
+		);
+	});
+
+	graph.fillColor = new Color(
+		new Gradient(gradientStops),
+		new Point(xStart, 0),
+		new Point(xEnd, 0)
+	);
+}
+
+function setCurve(points) {
+	if (points.length !== curveSegments.length) {
+		console.error("Invalid points array length");
+		return;
+	}
+	var tweenTo = {};
+	points.forEach(function (point, i) {
+		tweenTo["segments[" + i + "].point.x"] = point[0] * xScale;
+		tweenTo["segments[" + i + "].point.y"] = point[1] * yScale;
+	});
+
+	var elasticEaseOut = function (t) {
+		var c4 = (2 * Math.PI) / 3;
+		return t === 0
+			? 0
+			: t === 1
+				? 1
+				: Math.pow(2, -10 * t) * Math.sin((t * 10 - 0.75) * c4) + 1;
+	};
+
+	var tween = graph.tween(tweenTo, {
+		duration: 1000,
+		easing: elasticEaseOut,
+		start: false
+	});
+	tween.onUpdate = function () {
+		refreshGraph();
+	};
+	tween.start();
+}
+
+var xAxis = new Path({
 	segments: [
 		[25, 275],
 		[375, 275]
@@ -77,6 +162,21 @@ var referenceLines = new Group([
 	createReferenceLine(375, "08:00")
 ]);
 
+function yToTemperature(y) {
+	return Math.abs(Math.round(((y / yScale) * 17) / 250) - 2 - 30);
+}
+
+function refreshGraph() {
+	smoothen();
+	createGradient();
+	xAxis.bringToFront();
+	referenceLines.sendToBack();
+	curveSegments.forEach(function (segment) {
+		referenceLines.children[curveSegments.indexOf(segment)].lastChild.content =
+			yToTemperature(segment.point.y);
+	});
+}
+
 var maxTemp = new PointText({
 	point: [20, 30],
 	content: "30",
@@ -98,86 +198,29 @@ var minTemp = new PointText({
 	fillColor: "grey"
 });
 
-function smoothFillPath() {
-	var i;
-	for (i = 0; i < fillPath.segments.length; i++) {
-		if (i > 0 && i < graph.segments.length - 1) {
-			// Smooth only the middle curve segments
-			fillPath.segments[i].smooth({ type: "catmull-rom" });
-		} else {
-			// Keep first curve point, last curve point, and bottom segments sharp
-			fillPath.segments[i].smooth = false;
-		}
-	}
+var temperatures = new Group([maxTemp, midTemp, minTemp]);
+
+var allElements = new Group([graph, xAxis, temperatures, referenceLines]);
+
+function rescale(size) {
+	// Calculate new absolute scale factors
+	var newXScale = size.width / 400;
+	var newYScale = size.height / 300;
+	// Apply the relative scaling
+	allElements.scale(newXScale / xScale, newYScale / yScale, new Point(0, 0));
+	// Update the current scale factors
+	xScale = newXScale;
+	yScale = newYScale;
 }
 
-smoothFillPath();
+rescale(view.size);
 
-var allElements = new Group([
-	graph,
-	axes,
-	fillPath,
-	maxTemp,
-	midTemp,
-	minTemp,
-	referenceLines
-]);
+refreshGraph();
 
-allElements.scale(xScale, yScale, new Point(0, 0));
-
-function createHorizontalGradient() {
-	var gradientStops = [];
-	var curveSegments = fillPath.segments.slice(0, 6);
-	var xStart = curveSegments[0].point.x;
-	var xEnd = curveSegments[curveSegments.length - 1].point.x;
-	var xRange = xEnd - xStart;
-
-	// Define base colors
-	var orange = new Color(1.0, 1.0, 0.0); // strong orange
-	var blue = new Color(0.0, 0.4, 1.0); // strong blue
-	var neutral = new Color(0.9, 0.1, 0.0); // warm gray/neutral
-
-	curveSegments.forEach(function (segment) {
-		var point = segment.point;
-		var t = point.y / (170 * yScale);
-		t = Math.max(0, Math.min(1, t));
-
-		var r, g, b, f;
-
-		// First half: orange → neutral
-		if (t < 0.5) {
-			f = t / 0.5;
-			r = orange.red + (neutral.red - orange.red) * f;
-			g = orange.green + (neutral.green - orange.green) * f;
-			b = orange.blue + (neutral.blue - orange.blue) * f;
-		}
-		// Second half: neutral → blue
-		else {
-			f = (t - 0.5) / 0.5;
-			r = neutral.red + (blue.red - neutral.red) * f;
-			g = neutral.green + (blue.green - neutral.green) * f;
-			b = neutral.blue + (blue.blue - neutral.blue) * f;
-		}
-
-		var color = new Color(r, g, b);
-		var offset = (point.x - xStart) / xRange;
-		gradientStops.push(new GradientStop(color, offset));
-	});
-
-	var gradient = new Gradient(gradientStops);
-	fillPath.fillColor = new Color(
-		gradient,
-		new Point(xStart, 0),
-		new Point(xEnd, 0)
-	);
-}
-
-createHorizontalGradient();
-
-function closestSegment(path, point) {
-	var closestSegment = path.segments[0];
+function closestCurveSegment(point) {
+	var closestSegment = curveSegments[0];
 	var minDistance;
-	path.segments.forEach(function (segment) {
+	curveSegments.forEach(function (segment) {
 		var distance = Math.abs(segment.point.x - point.x);
 		if (distance < minDistance || minDistance === undefined) {
 			minDistance = distance;
@@ -187,64 +230,25 @@ function closestSegment(path, point) {
 	return closestSegment;
 }
 
-function yToTemperature(y) {
-	return Math.abs(Math.round(((y / yScale) * 17) / 250) - 2 - 30);
-}
-
 view.onMouseDrag = function (event) {
-	var segment = closestSegment(graph, event.point);
+	var segment = closestCurveSegment(event.point);
 	var newY = Math.max(Math.min(event.point.y, 275 * yScale), 25 * yScale);
 	segment.point.y = newY;
-	graph.smooth({ type: "catmull-rom" });
-	var index = graph.segments.indexOf(segment);
-	fillPath.segments[index].point.y = newY;
-	referenceLines.children[index].lastChild.content = yToTemperature(newY);
-	smoothFillPath();
-	createHorizontalGradient();
-	axes.bringToFront();
+	curveSegments[curveSegments.indexOf(segment)].point.y = newY;
+	refreshGraph();
 };
 
 view.onResize = function (event) {
-	// Calculate new absolute scale factors
-	var newXScale = event.size.width / 400;
-	var newYScale = event.size.height / 300;
-
-	// Apply the relative scaling
-	allElements.scale(newXScale / xScale, newYScale / yScale, new Point(0, 0));
-
-	// Update the current scale factors
-	xScale = newXScale;
-	yScale = newYScale;
-
-	referenceLines.sendToBack();
+	rescale(event.size);
 };
 
-view.onFrame = function (_event) {
-	TWEEN.update();
-};
+setCurve([
+	[25, 100],
+	[95, 80],
+	[165, 150],
+	[235, 200],
+	[305, 200],
+	[375, 150]
+]);
 
-axes.bringToFront();
-
-graph.segments.forEach(function (segment) {
-	referenceLines.children[graph.segments.indexOf(segment)].lastChild.content =
-		yToTemperature(segment.point.y);
-});
-
-globalThis.setGraphPoints = function (points) {
-	var i;
-	for (i = 0; i < points.length; i++) {
-		(function (segment, fillSegment, target) {
-			new TWEEN.Tween({ x: segment.point.x, y: segment.point.y })
-				.to({ x: target[0] * xScale, y: target[1] * yScale }, 500)
-				.easing(TWEEN.Easing.Elastic.Out)
-				.onUpdate(function (obj) {
-					segment.point.set(obj.x, obj.y);
-					fillSegment.point.set(obj.x, obj.y);
-					graph.smooth({ type: "catmull-rom" });
-					smoothFillPath();
-					createHorizontalGradient();
-				})
-				.start();
-		})(graph.segments[i], fillPath.segments[i], points[i]);
-	}
-};
+globalThis.setCurve = setCurve;
--- ui/app.tsx
+++ ui/app.tsx
@@ -12,7 +12,6 @@ const Layout: FC = (props) => {
 					type="text/javascript"
 					src="https://cdnjs.cloudflare.com/ajax/libs/paper.js/0.12.18/paper-full.min.js"
 				/>
-				<script src="https://cdnjs.cloudflare.com/ajax/libs/tween.js/23.1.3/tween.umd.js" />
 				<script type="text/paperscript" canvas="graph" src="paperscript.js" />
 				<link rel="stylesheet" href="style.css" />
 				<title>FreeSleep</title>
@@ -28,12 +27,6 @@ export const App: FC = () => {
 			<h1>Eight Sleep</h1>
 			<p>Set the temperature for your sleep cycle</p>
 			<canvas id="graph" resize></canvas>
-			<button
-				onClick="setGraphPoints([[25, 25],[95, 50],[165, 100],[235, 100],[305, 200],[375, 100]])"
-				type="button"
-			>
-				Preset
-			</button>
 		</Layout>
 	);
 };
